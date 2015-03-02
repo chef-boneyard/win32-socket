ceb64569e68ff7b88c67e2e5d882b74376060cdd
Daniel Berger <djberg96@gmail.com>
2015-03-02 06:14:36 -0700

    Started work on constructor.

--- lib/win32/socket.rb
+++ lib/win32/socket.rb
@@ -9,7 +9,29 @@ module Win32
     extend FFI::Library
     ffi_lib :ws2_32
 
-    def initialize
+    include Windows::WSASocketConstants
+
+    attr_reader :address_family
+    attr_reader :socket_type
+    attr_reader :protocol
+
+    # Example:
+    #
+    # socket = WSASocket.new(
+    #   :address_family => WSASocket::AF_INET,
+    #   :socket_type    => WSASocket::SOCK_STREAM,
+    #   :protocol       => WSASocket::IPPROTO_TCP
+    # )
+    def initialize(args = {})
+      @address_family = args[:address_family] || AF_INET
+      @socket_type = args[:socket_type] || SOCK_STREAM
+      @protocol = args[:protocol] || IPPROTO_TCP
     end
   end
+end
+
+if $0 == __FILE__
+  include Win32
+  s = WSASocket.new
+  p s
 end
