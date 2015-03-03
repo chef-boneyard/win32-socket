dad3057f0132a51db1dbfcdf84a3a18bea7c1c1e
Daniel Berger <djberg96@gmail.com>
2015-03-03 07:07:55 -0700

    Added more constructor options.

--- lib/win32/socket.rb
+++ lib/win32/socket.rb
@@ -12,35 +12,61 @@ module Win32
     attr_reader :address_family
     attr_reader :socket_type
     attr_reader :protocol
+    attr_reader :protocol_info
     attr_reader :group
     attr_reader :flags
+    attr_reader :port
+    attr_reader :address
 
     # Example:
     #
     # socket = WSASocket.new(
+    #   :address        => '127.0.0.1',
+    #   :port           => 3301,
     #   :address_family => WSASocket::AF_INET,
     #   :socket_type    => WSASocket::SOCK_STREAM,
-    #   :protocol       => WSASocket::IPPROTO_TCP
+    #   :protocol       => WSASocket::IPPROTO_TCP,
+    #   :group          => WSASocket::SG_UNCONSTRAINED_GROUP,
+    #   :flags          => WSASocket::WSA_FLAG_OVERLAPPED | WSASocket::WSA_NO_HANDLE_INHERIT
     # )
     def initialize(args = {})
-      @address_family = args.delete(:address_family) || AF_INET
+      @address_family  = args.delete(:address_family) || AF_INET
       @socket_type = args.delete(:socket_type) || SOCK_STREAM
       @protocol = args.delete(:protocol) || IPPROTO_TCP
       @group = args.delete(:group) || 0
       @flags = args.delete(:flags) || WSA_FLAG_OVERLAPPED
 
-      # Pass remaining members to WSAPROTOCOL_INFO
+      # Mandatory
+      @port = args.delete(:port)
+      @address = args.delete(:address)
 
-      @socket = WSASocketA(@address_family, @socket_type, @protocol, nil, @group, @flags)
+      # Optional
+      @protocol_info = args.delete(:protocol_info)
+
+      @socket = WSASocketA(
+        @address_family,
+        @socket_type,
+        @protocol,
+        @protocol_info,
+        @group,
+        @flags
+      )
 
       if @socket == INVALID_SOCKET_VALUE
         raise SystemCallError.new('WSASocket', WSAGetLastError())
       end
     end
 
+    def connect
+    end
+
     def close
       if closesocket(@socket) == SOCKET_ERROR
         raise SystemCallError.new("closesocket", WSAGetLastError())
+      end
+
+      if WSACleanup() == SOCKET_ERROR
+        raise SystemCallError.new("WSACleanup", WSAGetLastError())
       end
     end
   end
