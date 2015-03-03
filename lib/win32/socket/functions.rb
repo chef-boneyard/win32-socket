6e9d2348f51b34a3fe31cafa0d6d729e73012a55
Daniel Berger <djberg96@gmail.com>
2015-03-03 06:46:35 -0700

    Added more WSA functions.

--- lib/win32/socket/functions.rb
+++ lib/win32/socket/functions.rb
@@ -9,6 +9,8 @@ module Windows
     typedef :uintptr_t, :socket
 
     attach_function :WSASocketA, [:int, :int, :int, :pointer, :int, :dword], :socket
+    attach_function :WSACleanup, [], :int
+    attach_function :WSAConnectEx, [:socket, :pointer, :int, :pointer, :pointer, :pointer, :pointer], :int
     attach_function :WSAGetLastError, [], :int
     attach_function :closesocket, [:socket], :int
   end
