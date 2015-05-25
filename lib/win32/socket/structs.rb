1f3e376463e962e59083e645b81e7ce756675b4c
Daniel Berger <djberg96@gmail.com>
2015-05-25 00:15:35 -0600

    Fixes to the WSAPROTOCOL_INFO struct.

--- lib/win32/socket/structs.rb
+++ lib/win32/socket/structs.rb
@@ -49,35 +49,36 @@ module Windows
       layout(:tv_sec, :long, :tv_usec, :long)
     end
 
+    MAX_PROTOCOL_CHAIN = 7
+    WSAPROTOCOL_LENGTH = 256
+
+    class WSAPROTOCOL_CHAIN < FFI::Struct
+      layout(:ChainLen, :int, :ChainEntries, [:dword, MAX_PROTOCOL_CHAIN])
+    end
+
     class WSAPROTOCOL_INFO < FFI::Struct
-      extend FFI::Library
-
-      typedef :ulong, :dword
-
-      class WSAPROTOCOL_INFO < FFI::Struct
-        layout(
-          :dwServiceFlags1, :dword,
-          :dwServiceFlags2, :dword,
-          :dwServiceFlags3, :dword,
-          :dwServiceFlags4, :dword,
-          :dwProviderFlags, :dword,
-          :ProviderID, :dword,
-          :dwCatalogEntryId, :dword,
-          :ProtocolChain, :dword,
-          :iVersion, :int,
-          :iAddressFamily, :int,
-          :iMaxSockAddr, :int,
-          :iMinSockAddr, :int,
-          :iSocketType, :int,
-          :iProtocol, :int,
-          :iProtocolMaxOffset, :int,
-          :iNetworkByteOrder, :int,
-          :iSecurityScheme, :int,
-          :dwMessageSize, :dword,
-          :dwProviderReserved, :dword,
-          :szProtocol, [:char, 512]
-        )
-      end
+      layout(
+        :dwServiceFlags1, :dword,
+        :dwServiceFlags2, :dword,
+        :dwServiceFlags3, :dword,
+        :dwServiceFlags4, :dword,
+        :dwProviderFlags, :dword,
+        :ProviderID, GUID,
+        :dwCatalogEntryId, :dword,
+        :ProtocolChain, WSAPROTOCOL_CHAIN,
+        :iVersion, :int,
+        :iAddressFamily, :int,
+        :iMaxSockAddr, :int,
+        :iMinSockAddr, :int,
+        :iSocketType, :int,
+        :iProtocol, :int,
+        :iProtocolMaxOffset, :int,
+        :iNetworkByteOrder, :int,
+        :iSecurityScheme, :int,
+        :dwMessageSize, :dword,
+        :dwProviderReserved, :dword,
+        :szProtocol, [:char, WSAPROTOCOL_LENGTH + 1]
+      )
     end
 
     class WSANAMESPACE_INFO < FFI::Struct
