1e208ac3b1c2b056cbc724993c43384de542e3e5
Daniel Berger <djberg96@gmail.com>
2015-05-24 18:10:59 -0600

    Added WSAStartup function.

--- lib/win32/socket/functions.rb
+++ lib/win32/socket/functions.rb
@@ -8,14 +8,17 @@ module Windows
     typedef :ulong, :dword
     typedef :uintptr_t, :socket
     typedef :pointer, :ptr
+    typedef :ushort, :word
 
-    attach_function :WSASocketA, [:int, :int, :int, :ptr, :int, :dword], :socket
+    attach_function :closesocket, [:socket], :int
+    attach_function :inet_addr, [:string], :ulong
+
     attach_function :WSACleanup, [], :int
     attach_function :WSAConnect, [:socket, :ptr, :int, :ptr, :ptr, :ptr, :ptr], :int
     attach_function :WSAConnectByNameA, [:socket, :string, :string, :ptr, :ptr, :ptr, :ptr, :ptr, :ptr], :bool
-    attach_function :WSAEnumNameSpaceProviders, [:ptr, :ptr], :int
+    attach_function :WSAEnumNameSpaceProvidersA, [:ptr, :ptr], :int
     attach_function :WSAGetLastError, [], :int
-    attach_function :closesocket, [:socket], :int
-    attach_function :inet_addr, [:string], :ulong
+    attach_function :WSASocketA, [:int, :int, :int, :ptr, :int, :dword], :socket
+    attach_function :WSAStartup, [:word, :ptr], :int
   end
 end
