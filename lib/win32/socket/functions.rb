4c00ed934d32bacb788a5f4f2a3edb87ee080d1d
Daniel Berger <djberg96@gmail.com>
2015-05-31 18:46:19 -0600

    Added proto functions and SleepEx.

--- lib/win32/socket/functions.rb
+++ lib/win32/socket/functions.rb
@@ -3,16 +3,23 @@ require 'ffi'
 module Windows
   module WSASocketFunctions
     extend FFI::Library
-    ffi_lib :ws2_32
 
     typedef :ulong, :dword
     typedef :uintptr_t, :socket
     typedef :pointer, :ptr
     typedef :ushort, :word
 
+    ffi_lib :kernel32
+
+    attach_function :SleepEx, [:dword, :bool], :dword
+
+    ffi_lib :ws2_32
+
     attach_function :closesocket, [:socket], :int
     attach_function :inet_addr, [:string], :ulong
 
+    attach_function :WSAAsyncGetProtoByName, [:uintptr_t, :uint, :string, :buffer_out, :pointer], :uintptr_t
+    attach_function :WSAAsyncGetProtoByNumber, [:uintptr_t, :uint, :int, :buffer_out, :pointer], :uintptr_t
     attach_function :WSACleanup, [], :int
     attach_function :WSAConnect, [:socket, :ptr, :int, :ptr, :ptr, :ptr, :ptr], :int
     attach_function :WSAConnectByNameA, [:socket, :string, :string, :ptr, :ptr, :ptr, :ptr, :ptr, :ptr], :bool
