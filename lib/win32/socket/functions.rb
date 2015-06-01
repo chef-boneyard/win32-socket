1393bf3a70787c78b09d791f99520200fe7e5c4e
Daniel Berger <djberg96@gmail.com>
2015-06-01 17:55:12 -0600

    Fixed function prototype.

--- lib/win32/socket/functions.rb
+++ lib/win32/socket/functions.rb
@@ -18,7 +18,7 @@ module Windows
     attach_function :closesocket, [:socket], :int
     attach_function :inet_addr, [:string], :ulong
 
-    attach_function :WSAAsyncGetHostByAddr, [:uintptr_t, :uint, :string, :buffer_out, :pointer], :uintptr_t
+    attach_function :WSAAsyncGetHostByAddr, [:uintptr_t, :uint, :string, :int, :int, :buffer_out, :int], :uintptr_t
     attach_function :WSAAsyncGetHostByName, [:uintptr_t, :uint, :string, :buffer_out, :pointer], :uintptr_t
     attach_function :WSAAsyncGetProtoByName, [:uintptr_t, :uint, :string, :buffer_out, :pointer], :uintptr_t
     attach_function :WSAAsyncGetProtoByNumber, [:uintptr_t, :uint, :int, :buffer_out, :pointer], :uintptr_t
