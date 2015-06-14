affe1b909a16d75d091331b619a2a38a6790514b
Daniel Berger <djberg96@gmail.com>
2015-06-14 08:37:22 -0600

    Added more functions.

--- lib/win32/socket/functions.rb
+++ lib/win32/socket/functions.rb
@@ -17,9 +17,12 @@ module Windows
     ffi_lib :ws2_32
 
     attach_function :closesocket, [:socket], :int
+    attach_function :FreeAddrInfoEx, [:pointer], :void
     attach_function :inet_addr, [:string], :ulong
     attach_function :GetAddrInfo, :getaddrinfo, [:string, :string, :pointer, :pointer], :int
     attach_function :GetAddrInfoW, [:buffer_in, :buffer_in, :pointer, :pointer], :int
+    
+    attach_function :GetAddrInfoExA, [:string, :string, :dword, :ptr, :ptr, :ptr, :ptr, :ptr, :ptr, :ptr], :int
     attach_function :GetProtoByName, :getprotobyname, [:string], :ptr
     attach_function :GetProtoByNumber, :getprotobynumber, [:int], :ptr
 
@@ -38,5 +41,13 @@ module Windows
     attach_function :WSAGetLastError, [], :int
     attach_function :WSASocketA, [:int, :int, :int, :ptr, :int, :dword], :socket
     attach_function :WSAStartup, [:word, :ptr], :int
+
+    # Windows 8 or later
+    begin
+      attach_function :GetAddrInfoExCancel, [:pointer], :int
+      attach_function :GetAddrInfoExOverlappedResult, [:pointer], :int
+    rescue FFI::NotFoundError
+      # Do nothing
+    end
   end
 end
