0b66df16b1dab1c7a896cdc5bae772c79f3153e3
Daniel Berger <djberg96@gmail.com>
2015-06-22 19:40:03 -0600

    Added winerr helper methods.

--- lib/win32/socket/helper.rb
+++ lib/win32/socket/helper.rb
@@ -15,3 +15,38 @@ class FFI::Pointer
     elements
   end
 end
+
+module FFI
+  extend FFI::Library
+
+  ffi_lib :kernel32
+
+  attach_function :FormatMessage, :FormatMessageA,
+    [:ulong, :pointer, :ulong, :ulong, :pointer, :ulong, :pointer], :ulong
+
+  # Returns a Windows specific error message based on +err+ prepended
+  # with the +function+ name. Note that this does not actually raise
+  # an error, it only returns the message.
+  #
+  # The message will always be English regardless of your locale.
+  #
+  def win_error(function, err=FFI.errno)
+    flags = 0x00001000 | 0x00000200 # ARGUMENT_ARRAY + SYSTEM
+    buf = FFI::MemoryPointer.new(:char, 1024)
+
+    # 0x0409 = MAKELANGID(LANG_ENGLISH, SUBLANG_ENGLISH_US)
+    FormatMessage(flags, nil, err , 0x0409, buf, 1024, nil)
+
+    function + ': ' + buf.read_string.strip
+  end
+
+  # Raises a Windows specific error using SystemCallError that is based on
+  # the +err+ provided, with the message prepended with the +function+ name.
+  #
+  def raise_winerror(function, err=FFI.errno)
+    raise SystemCallError.new(win_error(function, err), err)
+  end
+
+  module_function :win_error
+  module_function :raise_winerror
+end
