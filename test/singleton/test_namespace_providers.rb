008b5df76d668b1d2b96d099ee5174c321a3fd5b
Daniel Berger <djberg96@gmail.com>
2016-03-26 14:34:24 -0600

    Added more namespace_provider tests.

--- test/singleton/test_namespace_providers.rb
+++ test/singleton/test_namespace_providers.rb
@@ -12,4 +12,25 @@ class WSASocketNamespaceProviders < Test::Unit::TestCase
   test "namepace_providers basic functionality" do
     assert_respond_to(WSASocket, :namespace_providers)
   end
+
+  test "namespace_providers accepts an optional boolean argument" do
+    assert_nothing_raised{ WSASocket.namespace_providers }
+    assert_nothing_raised{ WSASocket.namespace_providers(true) }
+    assert_nothing_raised{ WSASocket.namespace_providers(false) }
+  end
+
+  test "namespace_providers returns an array of strings by default" do
+    assert_kind_of(Array, WSASocket.namespace_providers)
+    assert_kind_of(String, WSASocket.namespace_providers.first)
+  end
+
+  test "namespace_providers returns an array of WSANAMESPACE_INFO structs in verbose mode" do
+    assert_kind_of(Array, WSASocket.namespace_providers(true))
+    assert_kind_of(WSASocket::WSANAMESPACE_INFO, WSASocket.namespace_providers(true).first)
+  end
+
+  test "namespace_providers returns some expected values" do
+    assert_true(WSASocket.namespace_providers.include?('Tcpip'))
+    assert_true(WSASocket.namespace_providers.include?('NTDS'))
+  end
 end
